1f5cb045e10f5ef37d93579627f5ef86abfc90b7
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-10-06 20:12:08 +0200

    Faker included on function, now it will call a random name. Autoload of composer added.

--- Hello.php
+++ Hello.php
@@ -1,11 +1,20 @@
 <?php
-
+require __DIR__ . "/vendor/autoload.php";
 
 class Hello
 {
+
+    /**
+     * Hello constructor.
+     */
+    public function __construct()
+    {
+        $this->faker = Faker\Factory::create();
+    }
+
     public function say_hello()
     {
-        echo "Hola mon!\n";
+        echo "Hola " . $this->faker->name . "!\n";
     }
 
 }
